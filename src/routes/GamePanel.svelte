bee0ba5f97bc999cae74e5a734f502ebc7b0705f
Leuan <leonvintzeanu@protonmail.com>
2023-02-19 22:11:16 +0200

    added game board logic

---
+++ src/routes/GamePanel.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    let counter = 1;
+    let numbers:string[] = Array(16); //displayed numbers
+    numbers = numbers.fill('x');
+    let openPositions:number[] = Array(16); //positions in the array that can be overwritten
+    openPositions = Array.from(Array(16).keys());
+
+    function addNumber() {
+        let position = openPositions[Math.floor(Math.random() * openPositions.length)]; //pick a random position
+        numbers[position] = counter.toString();
+        counter++;
+        openPositions = openPositions.filter((e)=>e!=position);  
+        
+    }
+
+    //fill the array with the first 8 numbers
+    for (let i = 0; i < 8; i++){
+    addNumber();
+    }
+
+    
+    function handleClick(id:number) {
+        numbers[id] = 'x';
+        openPositions = [...openPositions, id];
+        if(counter <= 20){
+            addNumber();
+        }
+        if(openPositions.length == 16){
+            alert('slay!')
+        }
+    }
+</script>
+
+<div class="button-container">
+    {#each numbers as buttonNumber, id}
+    <button on:click={()=>{handleClick(id)}}>{buttonNumber}</button>
+    {/each}
+</div>
+
+<style>
+    .button-container {        
+        display: grid;
+        gap: 2em 2em;
+        grid-template-columns: repeat(4, 3em);
+        grid-template-rows: repeat(4, 3em);
+    }
+</style>
