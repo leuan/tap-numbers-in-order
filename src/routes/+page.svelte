77de4ceac3a11990c485e108492edcde988c32ae
Leuan <leonvintzeanu@protonmail.com>
2023-02-20 23:18:02 +0200

    Added game timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<script>
+<script lang="ts">
 	import GamePanel from "./GamePanel.svelte";
+	import Timer from "./Timer.svelte";
+	let isStarted = false;
+	let timer;
 </script>
+{#if isStarted}
+<Timer bind:this={timer}/>
 <GamePanel/>
+{:else}
+<button class="start-button" on:click={()=>{isStarted=true;}}>Start Game</button>
+{/if}
