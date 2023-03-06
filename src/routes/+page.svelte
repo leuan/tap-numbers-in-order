e1d1b0a73d88e8074d4538f2e69e390d97920ff5
Leuan <leonvintzeanu@protonmail.com>
2023-03-06 22:09:02 +0200

    added a restart game button, added the static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import GamePanel from "./GamePanel.svelte";
-	import Timer from "./Timer.svelte";
+	import GamePanel from './GamePanel.svelte';
+	import Timer from './Timer.svelte';
 	let isStarted = false;
 	let isFinished = false;
 	let timer: Timer;
-	let timeElapsed:number;
+	let timeElapsed: number;
 
-	function handleFinished(){
+	function handleFinished() {
 		timer.stopTimer();
 		isFinished = true;
 	}
@@ -22,19 +22,31 @@
 <main>
 	{#if isStarted}
 		<div class="flex justify-center">
-			<Timer bind:this={timer} bind:totalSeconds={timeElapsed}/>
+			<Timer bind:this={timer} bind:totalSeconds={timeElapsed} />
 		</div>
 		{#if !isFinished}
-			<GamePanel on:finished={handleFinished}/>
+			<GamePanel on:finished={handleFinished} />
 		{:else}
 			<div class="grid place-items-center">
-				<p class="text-3xl mx-2 mb-3">Great! You finished in {#if Math.floor(timeElapsed / 60) > 0}{Math.floor(timeElapsed / 60)} minutes and {/if}{timeElapsed % 60} seconds.</p>
-				<img src="/images/monkey.gif" alt="monkey"/>
+				<p class="text-3xl mx-2 mb-3">
+					Great! You finished in {#if Math.floor(timeElapsed / 60) > 0}{Math.floor(
+							timeElapsed / 60
+						)} minutes and
+					{/if}{timeElapsed % 60} seconds.
+				</p>
+				<button
+				class="px-6 py-4 bg-orange-400 rounded text-gray-50 shadow font-bold text-4xl mb-5"
+				on:click={() => {isFinished = false; timer.restartTimer();}}>Restart game</button>
+				<img src="/images/monkey.gif" alt="monkey" class="rounded w-3/4 md:w-1/2 lg:w-1/4"/>
 			</div>
 		{/if}
 	{:else}
 		<div class="flex justify-center items-center h-96">
-			<button class="px-6 py-4 bg-gray-400 rounded text-gray-50 shadow font-bold text-4xl" on:click={()=>{isStarted=true;}}>Start Game</button>
+			<button
+				class="px-6 py-4 bg-orange-400 rounded text-gray-50 shadow font-bold text-4xl"
+				on:click={() => {
+					isStarted = true;
+				}}>Start Game</button>
 		</div>
 	{/if}
 </main>
--- src/routes/GamePanel.svelte
+++ src/routes/GamePanel.svelte
@@ -1,47 +1,63 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-    const dispatch = createEventDispatcher();
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
-    let counter = 1;
-    let clickedCounter = 1;
-    let numbers:string[] = Array(16); //displayed numbers
-    numbers = numbers.fill('x');
-    let openPositions:number[] = Array(16); //positions in the array that can be overwritten
-    openPositions = Array.from(Array(16).keys());
+	let counter = 1;
+	let clickedCounter = 1;
+	let numbers: string[] = Array(16); //displayed numbers
+	numbers = numbers.fill('x');
+	let openPositions: number[] = Array(16); //positions in the array that can be overwritten
+	openPositions = Array.from(Array(16).keys());
 
-    const colors = ['#fc0303', '#fc5603', '#fcce03', '#4dff00', '#0026ff', '#a200ff', '#ff00ff', '#00eeff', '#4400ff'];
+	const colors = [
+		'#fc0303',
+		'#fc5603',
+		'#fcce03',
+		'#4dff00',
+		'#0026ff',
+		'#a200ff',
+		'#ff00ff',
+		'#00eeff',
+		'#4400ff'
+	];
 
-    function addNumber() {
-        let position = openPositions[Math.floor(Math.random() * openPositions.length)]; //pick a random position
-        numbers[position] = counter.toString();
-        counter++;
-        openPositions = openPositions.filter((e)=>e!=position);  
-        
-    }
+	function addNumber() {
+		let position = openPositions[Math.floor(Math.random() * openPositions.length)]; //pick a random position
+		numbers[position] = counter.toString();
+		counter++;
+		openPositions = openPositions.filter((e) => e != position);
+	}
 
-    //fill the array with the first 8 numbers
-    for (let i = 0; i < 8; i++){
-    addNumber();
-    }
+	//fill the array with the first 8 numbers
+	for (let i = 0; i < 8; i++) {
+		addNumber();
+	}
 
-    
-    function handleClick(id:number) {
-        if(numbers[id] === clickedCounter.toString()){
-            clickedCounter++;
-            numbers[id] = 'x';
-            openPositions = [...openPositions, id];
-            if(counter <= 20){
-                addNumber();
-            }
-            if(clickedCounter == 21){
-                dispatch('finished');
-            }
-        }
-    }
+	function handleClick(id: number) {
+		if (numbers[id] === clickedCounter.toString()) {
+			clickedCounter++;
+			numbers[id] = 'x';
+			openPositions = [...openPositions, id];
+			if (counter <= 20) {
+				addNumber();
+			}
+			if (clickedCounter == 21) {
+				dispatch('finished');
+			}
+		}
+	}
 </script>
 
-<div class="grid grid-cols-[repeat(4,_minmax(3rem,_1fr))] md:grid-cols-[repeat(4,_15%)] xl:grid-cols-[repeat(4,_7%)] place-content-center gap-2">
-    {#each numbers as buttonNumber, id}
-    <button style="color: {colors[Math.floor(Math.random() * 9)]};" class="bg-gray-200 aspect-square rounded text-4xl font-bold" on:click={()=>{handleClick(id)}}>{buttonNumber === 'x' ? '' : buttonNumber}</button>
-    {/each}
+<div
+	class="grid grid-cols-[repeat(4,_minmax(3rem,_1fr))] md:grid-cols-[repeat(4,_15%)] xl:grid-cols-[repeat(4,_7%)] place-content-center gap-2"
+>
+	{#each numbers as buttonNumber, id}
+		<button
+			style="color: {colors[Math.floor(Math.random() * 9)]};"
+			class="bg-gray-200 aspect-square rounded text-4xl font-bold"
+			on:click={() => {
+				handleClick(id);
+			}}>{buttonNumber === 'x' ? '' : buttonNumber}</button
+		>
+	{/each}
 </div>
